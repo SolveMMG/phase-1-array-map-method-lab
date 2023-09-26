c29e460c9eb1801404ebe6c7b635a1d18919965c
Your Name <mikachupichu@gmail.com>
2023-09-26 12:26:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial){
+    const words= tutorial.split(" ");
+    const titleCaseWords=words.map(function(word){
+      return word.charAt(0).toUpperCase()+word.slice(1);
+    });
+    return titleCaseWords.join(" ")
+  })
 }
